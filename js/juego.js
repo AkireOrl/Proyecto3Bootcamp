c3c07a38f32bd26b3a202876e46d20105ca90da9
erika <om.erikaorlando@gmail.com>
2023-11-27 10:17:15 +0100

    Intentando coger el color de fondo de un elemento y asignarlo a otro elemento con handleClick

--- js/juego.js
+++ js/juego.js
@@ -7,12 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // Convertir los colores almacenados de formato JSON a un array de JavaScript
         let combinationColors = JSON.parse(storedColors);
         console.log("estoy aqui");
-        // Asignar los colores recuperados a los elementos que desees
-        // Por ejemplo, si tienes cuadrados o elementos para mostrar los colores, puedes hacerlo así:
+        // Asignar los colores recuperados a los elementos
         combinationColors.forEach((color, index) => {
             let squareId = 'J' + (index + 1);
             let squareElement = document.getElementById(squareId);
-            
+
             if (squareElement) {
                 squareElement.style.backgroundColor = color;
             }
@@ -21,3 +20,37 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+function getBackgroundColor(element) {
+    let color = element.style.backgroundColor;
+   
+    if (!color) {
+       if (window.getComputedStyle) {
+         color = window.getComputedStyle(element).backgroundColor;
+       } else if (element.currentStyle) {
+         color = element.currentStyle.backgroundColor;
+       }
+    }
+   
+    return color;
+   }
+   
+   function changeBackgroundColor(elements, color) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = color;
+    }
+   }
+   
+   function handleClick(event) {
+    var sourceElement = event.currentTarget;
+    var destinationElements = document.getElementsByClassName("squarePlay");
+   
+    var sourceColor = getBackgroundColor(sourceElement);
+    changeBackgroundColor(destinationElements, sourceColor);
+   }
+   
+   // asociar la función handleClick al evento click del elemento con id sourceElement
+   var squareColors = document.getElementsByClassName("squareColors");
+   for (var i = 0; i < squareColors.length; i++) {
+       squareColors[i].addEventListener("click", handleClick);
+   }
